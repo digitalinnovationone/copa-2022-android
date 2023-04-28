9a8e5881382d9047a12eb60c1a20cb7423e92754
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-04-27 21:05:10 -0300

    Finalizado MainScreen

--- app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Copa2022Theme {
                val state by viewModel.state.collectAsState()
-                Log.e("TAG", "onCreate: ${state.matches}")
+                MainScreen(matches = state.matches)
             }
         }
     }
--- app/src/main/kotlin/me/dio/copa/catar/features/MainScreen.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainScreen.kt
@@ -5,13 +5,23 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import me.dio.copa.catar.domain.extensions.getDate
+import me.dio.copa.catar.domain.model.Match
 import me.dio.copa.catar.domain.model.MatchDomain
+import me.dio.copa.catar.domain.model.TeamDomain
 import me.dio.copa.catar.ui.theme.Shapes
 
 @Composable
@@ -19,7 +29,7 @@ fun MainScreen(matches: List<MatchDomain>) {
     Box(modifier = Modifier
         .fillMaxSize()
         .padding(8.dp)){
-        LazyColumn(){
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)){
             items(matches){match ->
                 MatchInfo(match)
             }
@@ -42,8 +52,8 @@ fun MatchInfo(match: MatchDomain) {
                 )
             Column(modifier = Modifier.padding(16.dp)) {
                 Notification(match)
-                Title()
-                Teams()
+                Title(match)
+                Teams(match)
             }
         }
     }
@@ -52,7 +62,7 @@ fun MatchInfo(match: MatchDomain) {
 
 @Composable
 fun Notification(match: MatchDomain) {
-    Row{
+    Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End){
         val drawable = if (match.notificationEnabled) me.dio.copa.catar.R.drawable.ic_notifications_active
         else me.dio.copa.catar.R.drawable.ic_notifications
         Image(
@@ -62,11 +72,54 @@ fun Notification(match: MatchDomain) {
 }
 
 @Composable
-fun Title() {
-
+fun Title(match: MatchDomain) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+        ) {
+        Text(
+            text = "${match.date.getDate()} - ${match.name}",
+            style = MaterialTheme.typography.h6.copy(color = Color.White)
+            )
+    }
 }
 
 @Composable
-fun Teams() {
+fun Teams(match : MatchDomain) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        TeamItem(team = match.team1)
 
+        Text(
+            text = "X",
+            modifier = Modifier.padding(end = 16.dp, start = 16.dp),
+            style = MaterialTheme.typography.h6.copy(color = Color.White)
+            )
+
+        TeamItem(team = match.team2)
+    }
+}
+
+@Composable
+fun TeamItem(team : TeamDomain){
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = team.flag,
+            modifier = Modifier.align(Alignment.CenterVertically),
+            style = MaterialTheme.typography.h3.copy(color = Color.White)
+        )
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Text(
+            text = team.displayName,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h6.copy(color = Color.White)
+        )
+    }
 }
